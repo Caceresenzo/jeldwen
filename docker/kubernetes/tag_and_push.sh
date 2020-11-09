94ea267916eaed226ccf365528fbb0f30b50a802
Caceresenzo <caceresenzo1502@gmail.com>
2020-11-09 02:09:37 +0100

    Updated tagging script

--- docker/kubernetes/tag_and_push.sh
+++ docker/kubernetes/tag_and_push.sh
@@ -1,5 +1,5 @@
 docker tag bind0000-mysql localhost:32000/bind0000-mysql
-docker tag jeldwen-glial localhost:32000/jeldwen-glial
+docker tag jeldwen localhost:32000/jeldwen
 
 docker push localhost:32000/bind0000-mysql
-docker push localhost:32000/jeldwen-glial
+docker push localhost:32000/jeldwen
